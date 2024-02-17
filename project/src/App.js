9d1ea348e8f0e49c74faea82f81016b8da7b6b99
Abdulrahman-Almufadaa <abdulrahmanalmufdda@gmail.com>
2024-02-17 23:01:40 +0300

    Navigation

--- project/src/App.js
+++ project/src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 import PropertyListings from './PropertyListings';
-import PropertyDetail from './PropertyDetail';
 import PropertyForm from './PropertyForm';
+import PropertyDetail from './PropertyDetail';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={PropertyListings} />
-        <Route path="/property/:id" component={PropertyDetail} />
-        <Route path="/property-form" component={PropertyForm} />
-      </Switch>
+      <div>
+        <nav>
+          <ul>
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/property-form">Add Property</NavLink></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={PropertyListings} />
+          <Route path="/property-form" component={PropertyForm} />
+          <Route path="/property/:id" component={PropertyDetail} />
+        </Switch>
+      </div>
     </Router>
   );
 };
 
 export default App;
+
 
