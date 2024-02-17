f611a97022c86eca9c723297e60d56fd39366f93
Abdulrahman-Almufadaa <abdulrahmanalmufdda@gmail.com>
2024-02-17 22:56:51 +0300

    Delete Property

--- project/src/PropertyDetail.js
+++ project/src/PropertyDetail.js
@@ -1,11 +1,11 @@
-import './App.css';
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 const PropertyDetail = () => {
   const { id } = useParams();
   const [property, setProperty] = useState(null);
+  const history = useHistory();
 
   useEffect(() => {
     axios.get(`https://65cd2742dd519126b840305e.mockapi.io/v1/hotels/${id}`)
@@ -17,20 +17,32 @@ const PropertyDetail = () => {
       });
   }, [id]);
 
+  const handleDelete = () => {
+    const confirmDelete = window.confirm('Are you sure you want to delete this property?');
+    if (confirmDelete) {
+      axios.delete(`https://65cd2742dd519126b840305e.mockapi.io/v1/hotels/${id}`)
+        .then(() => {
+          history.push('/');
+        })
+        .catch(error => {
+          console.error('Error deleting property: ', error);
+        });
+    }
+  };
+
   if (!property) {
     return <p>Loading...</p>;
   }
 
   return (
-    
     <div>
-      <h1 style={{paddingLeft:850}}>detailed page</h1>
       <h2>{property.title}</h2>
       <p>{property.description}</p>
       <p>Address: {property.address}</p>
       <p>Price: ${property.price}</p>
-
-      <h1>here is detailed part</h1>
+      
+      <h3>Here is the detailed Part</h3>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 };
